b6d121bc9637b74e6388ec58a1de2cb69bc4d3f7
JulyeAguiar <131816932+JulyeAguiar@users.noreply.github.com>
2023-12-12 17:20:06 -0400

    finalização dos detalhes

--- src/Componentes/detalhe.jsx
+++ src/Componentes/detalhe.jsx
@@ -3,21 +3,19 @@ import Card from "./Card";
 
 
 export default function Detalhe(){
-
+    
+    const {envia} = useParams()
+    const listas = JSON.parse(localStorage.getItem("Listas"))
+    const atv = listas.filter((video) => {
+        if(video.envia == envia){
+            return video
+        }
+        else{
+            return null
+        }
+    })
     return(
-    <h1>aaaaaa</h1>
-    // const {envia} = useParams()
-    // const listas = JSON.parse(localStorage.getItem("Listas"))
-    // const atv = listas.filter((video) => {
-    //     if(video.envia == envia){
-    //         return video
-    //     }
-    //     else{
-    //         return null
-    //     }
-    // })
-    // return(
-    //     <Card video = {atv=[0]} ></Card>
-    // )
+        <Card video = {atv[0]} ></Card>
     )
+    
 }
